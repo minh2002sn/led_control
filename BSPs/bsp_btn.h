4287751b1c55cb76718bbe958c2b166524c921c5
minh2002sn <minhphngnht@gmail.com>
2024-07-07 13:44:12 +0700

    feat(bsp_btn): Add button BSP layer

--- BSPs/bsp_btn.h
+++ BSPs/bsp_btn.h
@@ -19,8 +19,11 @@
 #define __BSP_BTN_
 
 /* Includes ----------------------------------------------------------- */
+#include <stdint.h>
 
 /* Public defines ----------------------------------------------------- */
+#define BSP_BTN_SUCCESS (0x00000000)
+#define BSP_BTN_ERROR   (0xFFFFFFFF)
 
 /* Public enumerate/structure ----------------------------------------- */
 
@@ -29,6 +32,36 @@
 /* Public variables --------------------------------------------------- */
 
 /* Public function prototypes ----------------------------------------- */
+/**
+ * @brief           Init buttons
+ *
+ * @return
+ *  - (0) : Success
+ *  - (-1): Error
+ */
+uint32_t bsp_btn_init();
+
+/**
+ * @brief           Button handling function in while(1) loop
+ *
+ * @return
+ *  - (0) : Success
+ *  - (-1): Error
+ */
+uint32_t bsp_btn_loop();
+
+/**
+ * @brief           External interrupt function to handle button event
+ *
+ * @param[in]       exti_line   External interrupt line
+ *
+ * @attention       Call this function in external interrupt handling function (in bsp_exti)
+ *
+ * @return
+ *  - (0) : Success
+ *  - (-1): Error
+ */
+uint32_t bsp_btn_exti_handle(uint16_t exti_line);
 
 #endif // __BSP_BTN_
 
