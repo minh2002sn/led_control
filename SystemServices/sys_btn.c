3441b7fe436cf1db27575395577595e68e63a5ef
minh2002sn <minhphngnht@gmail.com>
2024-07-08 00:42:04 +0700

    feat(sys_btn): Add sending message to system manager

--- SystemServices/sys_btn.c
+++ SystemServices/sys_btn.c
@@ -21,24 +21,25 @@
 #include "bsp_btn.h"
 #include "cbuffer.h"
 #include "common.h"
+#include "sys_mng.h"
 
 /* Private defines ---------------------------------------------------- */
-#define SYS_BTN_MSG_BUFF_SIZE 32 /* Size of mMessage circular buffer  */
+#define SYS_BTN_MSG_BUFF_SIZE (32) /* Size of mMessage circular buffer  */
 
 /* Private enumerate/structure ---------------------------------------- */
-typedef struct
+typedef struct __attribute__((packed))
 {
-  sys_btn_id_t btn_id   : 8;
-  sys_btn_event_t event : 8;
-} __attribute__((packed)) sys_btn_msg_t;
+  sys_btn_id_t    btn_id;
+  sys_btn_event_t event;
+} sys_btn_msg_t;
 
 /* Private macros ----------------------------------------------------- */
 
 /* Public variables --------------------------------------------------- */
-static cbuffer_t sys_btn_msg_cb;
-static uint8_t sys_btn_msg_buff[SYS_BTN_MSG_BUFF_SIZE];
 
 /* Private variables -------------------------------------------------- */
+static cbuffer_t sys_btn_msg_cb;
+static uint8_t   sys_btn_msg_buff[SYS_BTN_MSG_BUFF_SIZE];
 
 /* Private function prototypes ---------------------------------------- */
 
@@ -60,7 +61,40 @@ uint32_t sys_btn_init()
 
 uint32_t sys_btn_loop()
 {
+  sys_btn_msg_t msg;
+
+  // BSP loop function
   bsp_btn_loop();
+
+  // Read message from circular buffer
+  if ((cb_data_count(&sys_btn_msg_cb) / sizeof(sys_btn_msg_t)) >= 1)
+  {
+    cb_read(&sys_btn_msg_cb, &msg, sizeof(msg));
+
+    // Process message
+    switch (msg.event)
+    {
+    case SYS_BTN_EVENT_SINGLE_CLICK:
+      sys_mng_send_event(SYS_MNG_MSG_SRC_BTN, (sys_mng_event_t)SYS_MNG_BTN_EVENT_SINGLE_CLICK);
+      break;
+
+    case SYS_BTN_EVENT_DOUBLE_CLICK:
+      sys_mng_send_event(SYS_MNG_MSG_SRC_BTN, (sys_mng_event_t)SYS_MNG_BTN_EVENT_DOUBLE_CLICK);
+      break;
+
+    case SYS_BTN_EVENT_HOLD:
+      sys_mng_send_event(SYS_MNG_MSG_SRC_BTN, (sys_mng_event_t)SYS_MNG_BTN_EVENT_HOLD);
+      break;
+
+    case SYS_BTN_EVENT_RELEASE:
+      sys_mng_send_event(SYS_MNG_MSG_SRC_BTN, (sys_mng_event_t)SYS_MNG_BTN_EVENT_RELEASE);
+      break;
+
+    default:
+      break;
+    }
+  }
+
   return SYS_BTN_SUCCESS;
 }
 
--- SystemServices/sys_btn.h
+++ SystemServices/sys_btn.h
@@ -26,19 +26,18 @@
 #define SYS_BTN_ERROR   (0xFFFFFFFF)
 #define SYS_BTN_BUFFULL (0xFFFFFFFE) /* Buffer full */
 
-/* Public enumerate/structure ----------------------------------------- */
-typedef enum
-{
-  SYS_BTN_BTN0,
-} sys_btn_id_t;
 
-typedef enum
-{
-  SYS_BTN_EVENT_SINGLE_CLICK,
-  SYS_BTN_EVENT_DOUBLE_CLICK,
-  SYS_BTN_EVENT_HOLD,
-  SYS_BTN_EVENT_RELEASE
-} sys_btn_event_t;
+/* Public enumerate/structure ----------------------------------------- */
+// Type define enum sys_btn_id_t that has sizeof() = 1
+typedef uint8_t sys_btn_id_t;
+#define SYS_BTN_BTN0 ((sys_btn_id_t)0x00)
+
+// Type define enum sys_btn_event_t that has sizeof() = 1
+typedef uint8_t sys_btn_event_t;
+#define SYS_BTN_EVENT_SINGLE_CLICK ((sys_btn_event_t)0x00)
+#define SYS_BTN_EVENT_DOUBLE_CLICK ((sys_btn_event_t)0x01)
+#define SYS_BTN_EVENT_HOLD         ((sys_btn_event_t)0x02)
+#define SYS_BTN_EVENT_RELEASE      ((sys_btn_event_t)0x03)
 
 /* Public macros ------------------------------------------------------ */
 
