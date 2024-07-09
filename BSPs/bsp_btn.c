9318bfa1f1c49216ea6a70ab6fe5186c64d54478
minh2002sn <minhphngnht@gmail.com>
2024-07-09 13:18:52 +0700

    update(bsp_btn): Re-format

--- BSPs/bsp_btn.c
+++ BSPs/bsp_btn.c
@@ -22,6 +22,7 @@
 #include "drv_btn.h"
 #include "main.h"
 #include "sys_btn.h"
+#include "sys_prot.h"
 
 /* Private defines ---------------------------------------------------- */
 #define BTN_ACTIVE_STATE 1 /* Active state of buttons */
@@ -30,34 +31,51 @@
 typedef struct
 {
   GPIO_TypeDef *gpio_x;
-  uint16_t gpio_pin;
-  drv_btn_t btn;
+  uint16_t      gpio_pin;
+  drv_btn_t     btn;
 } bsp_led_data_t;
 
 /* Private macros ----------------------------------------------------- */
 
 /* Public variables --------------------------------------------------- */
-static bsp_led_data_t btns_data[] =
-{
+static bsp_led_data_t btns_data[] = {
   { GPIOA, GPIO_PIN_0 }
 };
 
 /* Private variables -------------------------------------------------- */
 
 /* Private function prototypes ---------------------------------------- */
-void bsp_btn_cb_func_single_click(drv_btn_t *btn);
-void bsp_btn_cb_func_double_click(drv_btn_t *btn);
-void bsp_btn_cb_func_hold(drv_btn_t *btn);
-void bsp_btn_cb_func_release(drv_btn_t *btn);
+/**
+ * @brief           Callback function of single click event
+ */
+static void bsp_btn_cb_func_single_click(drv_btn_t *btn);
+
+/**
+ * @brief           Callback function of double click event
+ */
+static void bsp_btn_cb_func_double_click(drv_btn_t *btn);
+
+/**
+ * @brief           Callback function of hold event
+ */
+static void bsp_btn_cb_func_hold(drv_btn_t *btn);
+
+/**
+ * @brief           Callback function of release event
+ */
+static void bsp_btn_cb_func_release(drv_btn_t *btn);
 
 /* Function definitions ----------------------------------------------- */
 uint32_t bsp_btn_init()
 {
   uint32_t ret;
+
+  // Initialize button GPIO pin, active state
   ret = drv_btn_init(&btns_data[0].btn, btns_data[0].gpio_x,
                      btns_data[0].gpio_pin, BTN_ACTIVE_STATE);
   ASSERT(ret == DRV_BTN_SUCCESS, BSP_BTN_ERROR);
 
+  // Register callback function for each button events
   ret = drv_btn_reg_cb_func(bsp_btn_cb_func_single_click, bsp_btn_cb_func_double_click,
                             bsp_btn_cb_func_hold, bsp_btn_cb_func_release);
   ASSERT(ret == DRV_BTN_SUCCESS, BSP_BTN_ERROR);
@@ -87,22 +105,26 @@ uint32_t bsp_btn_exti_handle(uint16_t exti_line)
 /* Private definitions ----------------------------------------------- */
 void bsp_btn_cb_func_single_click(drv_btn_t *btn)
 {
-  sys_btn_send_msg(0, SYS_BTN_EVENT_SINGLE_CLICK);
+  uint8_t data[] = { SYS_BTN_BTN0, SYS_BTN_EVENT_SINGLE_CLICK };
+  sys_prot_send_msg(SYS_PROT_NOTE_BSP_BTN, SYS_PROT_NOTE_SYS_BTN, data, sizeof(data));
 }
 
 void bsp_btn_cb_func_double_click(drv_btn_t *btn)
 {
-  sys_btn_send_msg(0, SYS_BTN_EVENT_DOUBLE_CLICK);
+  uint8_t data[] = { SYS_BTN_BTN0, SYS_BTN_EVENT_DOUBLE_CLICK };
+  sys_prot_send_msg(SYS_PROT_NOTE_BSP_BTN, SYS_PROT_NOTE_SYS_BTN, data, sizeof(data));
 }
 
 void bsp_btn_cb_func_hold(drv_btn_t *btn)
 {
-  sys_btn_send_msg(0, SYS_BTN_EVENT_HOLD);
+  uint8_t data[] = { SYS_BTN_BTN0, SYS_BTN_EVENT_HOLD };
+  sys_prot_send_msg(SYS_PROT_NOTE_BSP_BTN, SYS_PROT_NOTE_SYS_BTN, data, sizeof(data));
 }
 
 void bsp_btn_cb_func_release(drv_btn_t *btn)
 {
-  sys_btn_send_msg(0, SYS_BTN_EVENT_RELEASE);
+  uint8_t data[] = { SYS_BTN_BTN0, SYS_BTN_EVENT_RELEASE };
+  sys_prot_send_msg(SYS_PROT_NOTE_BSP_BTN, SYS_PROT_NOTE_SYS_BTN, data, sizeof(data));
 }
 
 /* End of file -------------------------------------------------------- */
