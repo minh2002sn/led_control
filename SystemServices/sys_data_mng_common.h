8371097bc1a21bb4cef00e4002038ea7325b58e8
minh2002sn <minhphngnht@gmail.com>
2024-07-10 14:27:42 +0700

    feat(sys_data_mng): Add system data manager topic definition

---
+++ SystemServices/sys_data_mng_common.h
@@ -0,0 +1,39 @@
+/**
+ * @file        sys_led.h
+ * @copyright
+ * @license
+ * @version     0.0.0
+ * @date
+ * @author      Minh Phung Nhat
+ *              Hung Pham Duc
+ *              Khoi Nguyen Thanh
+ * @brief       none
+ *
+ * @note        none
+ *
+ * @example     none
+ *
+ */
+/* Define to prevent recursive inclusion ------------------------------ */
+#ifndef __SYS_DATA_MNG_COMMON_
+#define __SYS_DATA_MNG_COMMON_
+
+/* Includes ----------------------------------------------------------- */
+#include <stdint.h>
+
+/* Public defines ----------------------------------------------------- */
+
+/* Public enumerate/structure ----------------------------------------- */
+typedef uint8_t sys_data_mng_topic_id_t;
+#define SYS_DATA_MNG_TOPIC_BTN ((sys_data_mng_topic_id_t)0x00)
+#define SYS_DATA_MNG_TOPIC_LED ((sys_data_mng_topic_id_t)0x01)
+
+/* Public macros ------------------------------------------------------ */
+
+/* Public variables --------------------------------------------------- */
+
+/* Public function prototypes ----------------------------------------- */
+
+#endif // __SYS_DATA_MNG_COMMON_
+
+/* End of file -------------------------------------------------------- */
